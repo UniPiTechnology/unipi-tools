f63d935ed59b27b537d0b1057c622d2c0c7e28cd
Miroslav Ondra <ondra@faster.cz>
2019-09-13 16:31:08 +0200

    Fix hook-functions - test before rm

--- unipi-common/etc/initramfs/hooks/netnames.sh
+++ unipi-common/etc/initramfs/hooks/netnames.sh
@@ -4,10 +4,8 @@ mkdir -p ${DESTDIR}/etc/systemd/network
 touch ${DESTDIR}/etc/systemd/network/99-default.link
 
 . /usr/share/initramfs-tools/hook-functions
-rm ${DESTDIR}/usr/sbin/mke2fs
+[ -f ${DESTDIR}/usr/sbin/mke2fs ] && rm ${DESTDIR}/usr/sbin/mke2fs
 copy_exec /sbin/mke2fs
 copy_exec /sbin/fdisk
 copy_exec /sbin/sfdisk
 copy_exec /sbin/mkfs.ext4
-
-
